c0893b0ea8e14098efc383170bc6c25159c7f04f
vesheff <martin.veshev@gmail.com>
2015-03-26 01:17:16 +0200

    Initial commit skeleton

---
+++ Loquat Mega Store/ClassLibrary1/ComputerAppliance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoquatMegaStore
+{
+    public abstract class ComputerAppliance : Item
+    {
+    }
+}
