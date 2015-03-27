e6ba4b5a48f084d0eeeabc222b8d0d6638858688
LyubaG <bubu12@gmail.com>
2015-03-27 06:26:03 +0200

    E-mail validation added

--- Loquat Mega Store/ClassLibrary1/ShoppingSystem/User.cs
+++ Loquat Mega Store/ClassLibrary1/ShoppingSystem/User.cs
@@ -5,15 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using LoquatMegaStore.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace LoquatMegaStore.ShoppingSystem
 {
-    public abstract class User: IPayable, IOrderable
+    public abstract class User : IPayable, IOrderable
     {
         private string userId;
         private string password;
         private string email;
-        
+
 
         protected User(string userId, string password, string email)
         {
@@ -38,8 +39,43 @@ namespace LoquatMegaStore.ShoppingSystem
 
         public string Email
         {
-            get { return email; }
-            set { email = value; }
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                if (ValidString(value))
+                {
+                    if (ValidEmail(value))
+                    {
+                        email = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid e-mail address.");
+                    }
+                }
+            }
+        }
+
+        private bool ValidEmail(string email)
+        {
+            string patternStrict = @"^(([^<>()[\]\\.,;:\s@\""]+"
+                                  + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+                                  + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+                                  + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
+                                  + @"[a-zA-Z]{2,}))$";
+            Regex regexStrict = new Regex(patternStrict);
+            bool emailValid = regexStrict.IsMatch(email); ;
+            return emailValid;
+        }
+
+        private bool ValidString(string input)
+        {
+            bool stringValid = !(string.IsNullOrEmpty(input));
+            return stringValid;
         }
 
         public void MakePayment()
