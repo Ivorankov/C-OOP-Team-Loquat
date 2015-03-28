bd5be13af931a0af8499b237ed3f7abf1e02e57b
Alex-Bubblemaster <shiatzu@gmail.com>
2015-03-28 15:44:36 +0200

    Hello/tesst

--- Loquat Mega Store/Program/Program.cs
+++ Loquat Mega Store/Program/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LoquatMegaStore.ShoppingSystem;
-using LoquatMegaStore;
 
-
+    //hello from Alex
 namespace Program
 {
     class Program
