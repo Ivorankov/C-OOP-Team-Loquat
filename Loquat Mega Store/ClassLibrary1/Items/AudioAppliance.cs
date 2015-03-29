7bccfafd165fbf7c66e40342ac6f6b2e8ab0211f
Plamen <plamenvto@gmail.com>
2015-03-29 22:34:10 +0300

    Update AudioAppliance.cs

--- Loquat Mega Store/ClassLibrary1/Items/AudioAppliance.cs
+++ Loquat Mega Store/ClassLibrary1/Items/AudioAppliance.cs
@@ -1,7 +1,48 @@
-﻿namespace LoquatMegaStore.Items
+namespace LoquatMegaStore.Items
 {
+    using System;
+    using LoquatMegaStore.Interfaces;
+    using LoquatMegaStore.Structures;
+    using LoquatMegaStore.Enumerators;
+
     public abstract class AudioAppliance : Item
     {
 
+        private int outputPower;
+
+        public int OutputPower
+        {
+            get { return this.outputPower; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("The output power must be bigger than 0!");
+                }
+                this.outputPower = value;
+            }
+        }
+
+        public AudioAppliance(string manufacturer, string model, decimal price, decimal weight,
+            int powerConsumption, int amountInStock, Dimensions dimensions, int outputPower, Color color, AudioFormat playModes)
+            : base(manufacturer, model, price, weight, powerConsumption, amountInStock, dimensions,color)
+        {
+            this.OutputPower = outputPower;
+        }
+
+        public AudioAppliance(string manufacturer, string model, decimal price, decimal weight, int powerConsumption, int amountInStock, Dimensions dimensions, int outputPower, Enumerators.Color color)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Price = price;
+            this.Weight = weight;
+            this.PowerConsumption = powerConsumption;
+            this.AmountInStock = amountInStock;
+            this.Dimensions = dimensions;
+            this.OutputPower = outputPower;
+            this.Color = color;
+        }
+
+        public Dimensions Dimensions { get; set; }
     }
 }
