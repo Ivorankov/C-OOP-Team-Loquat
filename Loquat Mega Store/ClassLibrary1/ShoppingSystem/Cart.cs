162d21b800cd54937cc25d70b5f7be99783a6793
Ivorankov <ivo_rankov@mail.bg>
2015-03-31 12:47:47 +0300

    AddItem method in Cart

--- Loquat Mega Store/ClassLibrary1/ShoppingSystem/Cart.cs
+++ Loquat Mega Store/ClassLibrary1/ShoppingSystem/Cart.cs
@@ -28,5 +28,9 @@
         {
 
         }
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
     }
 }
--- Loquat Mega Store/UI/WpfApplication1/ItemWindows/Computers/LaptopWindow.xaml.cs
+++ Loquat Mega Store/UI/WpfApplication1/ItemWindows/Computers/LaptopWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using LoquatMegaStore.Items;
 using LoquatMegaStore.Structures;
 using LoquatMegaStore.Enumerators;
+using LoquatMegaStore.ShoppingSystem;
 
 namespace WpfApplication1
 {
@@ -22,21 +23,20 @@ namespace WpfApplication1
     /// </summary>
     public partial class LaptopWindow : Window
     {
+        public Laptop laptop = new Laptop("Testt", "Mod", 1.12m, 23.21m, 120, 2, new Dimensions(2, 2, 2),
+                new Processor(4, 25.2, 6), new Display(12.2, DisplayType.TFT, DisplayResolution.p1024),
+                new Speaker(50.5, 4.5), 256, 1000, LoquatMegaStore.Enumerators.Color.Gray);
         public LaptopWindow()
         {
             InitializeComponent();
-            Laptop laptop = new Laptop("Testt", "Mod", 1.12m, 23.21m, 120, 2, new Dimensions(2, 2, 2),
-                new Processor(4, 25.2, 6), new Display(12.2, DisplayType.TFT, DisplayResolution.p1024),
-                new Speaker(50.5,4.5), 256, 1000, LoquatMegaStore.Enumerators.Color.Gray);
+
             TextBox.Text = laptop.ToString();
         }
 
         private void Button_Click_12(object sender, RoutedEventArgs e)
         {
-            //Laptop laptop = new Laptop("Testt", "MOdel", 1.12m, 23.21m, 120, 2, new Dimensions(2, 2, 2),
-            //    new Processor(4, 25.2, 6), new Display(12.2, DisplayType.TFT, DisplayResolution.p1024),
-            //    12.5, 14.2, 256, 1000, LoquatMegaStore.Enumerators.Color.Gray);
-            //laptop.AddToCart();
+            Cart cart = new Cart();
+            cart.AddItem(laptop);
         }
 
     }
